176dc31b4aa3c6a4012ac0f123076c92828c3a4f
emenamm <39969038+emenamm@users.noreply.github.com>
2019-11-04 00:06:47 +0100

    Update product-description.component.ts

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-
-  constructor() { }
+    albuminfo;
+  constructor(private _productservice : ProductService) { }
 
   ngOnInit() {
+    this._productservice.getAlbum(1).subscribe(response=>this.albuminfo=response);
   }
 
 }
